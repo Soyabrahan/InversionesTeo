1b85376bb58e063f9400e54da268ab10a8e28809
Abrahan <abrahanramirez2410@gmail.com>
2025-07-02 01:35:14 -0400

    arreglando error en despliegue 2

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export", // Comentado para permitir rutas dinámicas y SSR
   eslint: {
     ignoreDuringBuilds: true,
   },
