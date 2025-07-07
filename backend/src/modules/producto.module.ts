0cff5140f389bea0a0a34bb6c5cb5f601ffdd5b2
Abrahan <abrahanramirez2410@gmail.com>
2025-07-07 00:09:28 -0400

    bichos2

--- backend/src/modules/producto.module.ts
+++ backend/src/modules/producto.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Producto } from '../entities/producto.entity';
+import { Moneda } from '../entities/moneda.entity';
 import { ProductoService } from '../services/producto.service';
 import { ProductoController } from '../controllers/producto.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Producto])],
+  imports: [TypeOrmModule.forFeature([Producto, Moneda])],
   controllers: [ProductoController],
   providers: [ProductoService],
   exports: [TypeOrmModule],
